c4a88bf67914dc5c535234aedd90bbfa27176a8f
Eric David Smith <erictherobot@gmail.com>
2023-04-12 19:20:47 -0400

    Create Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+export default function Footer() {
+  return (
+    <footer className="bg-black text-white text-center py-4 rounded">
+      <p>
+        Made with ❤️ by{" "}
+        <a
+          href="https://ericdavidsmith.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="underline"
+        >
+          Eric David Smith
+        </a>
+      </p>
+    </footer>
+  );
+}
